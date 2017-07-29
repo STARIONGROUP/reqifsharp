0d804382be0a7d4b87542bee8fa890b036cd55ff
samatrhea <s.gerene@rheagroup.com>
2017-07-29 17:42:50 +0200

    [convert] test project to VS2017 csroj format [Update] authors of ReqIFSharp project

--- ReqIFSharp.Tests/Properties/AssemblyInfo.cs
+++ ReqIFSharp.Tests/Properties/AssemblyInfo.cs
@@ -22,18 +22,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ReqIFSharp.Tests")]
-[assembly: AssemblyDescription("ReqIFSharp Test Project")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RHEA System S.A.")]
-[assembly: AssemblyProduct("ReqIFSharp")]
-[assembly: AssemblyCopyright("Copyright 2017 RHEA System S.A.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -42,15 +30,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ff23d20c-55d5-4ec1-9db5-57f1c437c183")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
