d96203280ded07de2750971d0ac61f9652e2ba65
samatrhea <s.gerene@rheagroup.com>
2019-05-17 14:54:56 +0200

    [Add] sample_debug reqif file
    [Add] test to verify that XHTML attributes can be deserialized from sample_debug reqif file

---
+++ ReqIFSharp.Tests/SampleDebugTestFixture.cs
@@ -0,0 +1,59 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="SampleDebugTestFixture.cs" company="RHEA System S.A.">
+//
+//   Copyright 2017 RHEA System S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+namespace ReqIFSharp.Tests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using NUnit.Framework;
+    using ReqIFSharp;
+
+    [TestFixture]
+    public class SampleDebugTestFixture
+    {
+        [Test]
+        public void Verify_that_sampledebug_reqif_file_can_be_deserialized()
+        {
+            var deserializer = new ReqIFDeserializer();
+            var reqIf = deserializer.Deserialize(Path.Combine(TestContext.CurrentContext.TestDirectory, "sample_debug.reqif"));
+            var header = reqIf.TheHeader.First();
+            var content = reqIf.CoreContent.First();
+
+            Assert.That(header.Identifier, Is.EqualTo("_c1638633-9ec4-4f9f-aabd-4804f5e0110c")); 
+
+            var specObject = content.SpecObjects.Single(x => x.Identifier == "rmf-249cb10a-69cf-48ac-ac40-bcba7fc15d47");
+
+            foreach (var attributeValueXhtml in specObject.Values.OfType<AttributeValueXHTML>())
+            {
+                Assert.That(attributeValueXhtml.TheValue, Is.Not.Null.Or.Empty);
+                Console.WriteLine(attributeValueXhtml.TheValue);
+            }
+
+            var xhtmlAttributes = content.SpecObjects.SelectMany(x => x.Values.OfType<AttributeValueXHTML>()).ToList();
+
+            foreach (var attributeValueXhtml in xhtmlAttributes)
+            {
+                Assert.That(attributeValueXhtml.TheValue, Is.Not.Null.Or.Empty);
+                Console.WriteLine(attributeValueXhtml.TheValue);
+            }
+        }
+    }
+}
