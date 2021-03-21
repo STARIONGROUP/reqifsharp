9ac3f972f7c10066d35900671f128d41f99b12d4
samatrhea <s.gerene@rheagroup.com>
2021-03-21 12:18:03 +0100

    [Delete] unnecessary xsd files from test project; move test resources to Resources folder

--- ReqIFSharp.Tests/ReqIfSchemaResolverTestFixture.cs
+++ ReqIFSharp.Tests/ReqIfSchemaResolverTestFixture.cs
@@ -39,10 +39,10 @@ namespace ReqIFSharp.Tests
         public void VerifyThatReferencedSchemaCanBeLoaded()
         {
             var a = Assembly.GetExecutingAssembly();
-            var stream = a.GetManifestResourceStream("ReqIFSharp.Tests.driver.xsd");
+            var stream = a.GetManifestResourceStream("ReqIFSharp.Tests.Resources.driver.xsd");
             
-            XmlSchema schema = XmlSchema.Read(stream, this.ValidationEventHandler);
-            schema.Compile(this.ValidationEventHandler, new ReqIfSchemaResolver());
+            var schema = XmlSchema.Read(stream, this.ValidationEventHandler);
+            Assert.DoesNotThrow(() => schema.Compile(this.ValidationEventHandler, new ReqIfSchemaResolver()));
         }
 
         /// <summary>
