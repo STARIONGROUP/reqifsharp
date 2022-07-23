570bc999966040a0f7ebf11e67be6b5ef2f6da72
sam.gerene <s.gerene@rheagroup.com>
2022-07-23 17:05:40 +0200

    [Add] SpecRelationTypeExtensions.QueryReferencingSpecRelations

---
+++ ReqIFSharp.Extensions.Tests/ReqIFExtensions/SpecRelationTypeExtensionsTestFixture.cs
@@ -0,0 +1,67 @@
+﻿//  -------------------------------------------------------------------------------------------------
+//  <copyright file="SpecRelationTypeExtensionsTestFixture.cs" company="RHEA System S.A.">
+// 
+//    Copyright 2017-2022 RHEA System S.A.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+//  </copyright>
+//  -------------------------------------------------------------------------------------------------
+
+namespace ReqIFSharp.Extensions.Tests.ReqIFExtensions
+{
+    using System;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using ReqIFSharp.Extensions.ReqIFExtensions;
+    using ReqIFSharp.Extensions.Tests.TestData;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="SpecRelationTypeExtensions"/> class.
+    /// </summary>
+    [TestFixture]
+    public class SpecRelationTypeExtensionsTestFixture
+    {
+        private ReqIF reqIf;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var reqIfTestDataCreator = new ReqIFTestDataCreator();
+
+            this.reqIf = reqIfTestDataCreator.Create();
+        }
+
+        [Test]
+        public void Verify_that_QueryReferencingSpecRelations_returns_expected_results()
+        {
+            var specRelationType = (SpecRelationType)this.reqIf.CoreContent.SpecTypes.Single(x => x.Identifier == "specificationrelation");
+
+            var specObjects = specRelationType.QueryReferencingSpecRelations();
+
+            Assert.That(specObjects.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Verify_that_on_QueryReferencingSpecRelations_NullReferenceException_is_thrown_when_owning_ReqIFContent_is_not_set()
+        {
+            var specObjectType = new SpecRelationType();
+
+            Assert.That(() => specObjectType.QueryReferencingSpecRelations(),
+                Throws.Exception.TypeOf<NullReferenceException>()
+                    .With.Message.Contains("The owning ReqIFContent of the SpecRelationType is not set."));
+        }
+    }
+}
---
+++ ReqIFSharp.Extensions/ReqIFExtensions/SpecRelationTypeExtensions.cs
@@ -0,0 +1,57 @@
+﻿//  -------------------------------------------------------------------------------------------------
+//  <copyright file="SpecRelationTypeExtensions.cs" company="RHEA System S.A.">
+// 
+//    Copyright 2017-2022 RHEA System S.A.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+//  </copyright>
+//  -------------------------------------------------------------------------------------------------
+
+namespace ReqIFSharp.Extensions.ReqIFExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ReqIFSharp;
+
+    /// <summary>
+    /// The <see cref="SpecRelationTypeExtensions"/> class provides a number of extension methods for the <see cref="SpecRelationType"/> class
+    /// </summary>
+    public static class SpecRelationTypeExtensions
+    {
+        /// <summary>
+        /// Queries the <see cref="SpecRelation"/>s that are referencing the <see cref="SpecRelationType"/>
+        /// </summary>
+        /// <param name="specRelationType">
+        /// The subject <see cref="SpecRelationType"/>
+        /// </param>
+        /// <returns>
+        /// An <see cref="IEnumerable{SpecRelation}"/> that are referencing the <see cref="SpecRelationType"/>
+        /// </returns>
+        public static IEnumerable<SpecRelation> QueryReferencingSpecRelations(this SpecRelationType specRelationType)
+        {
+            if (specRelationType.ReqIFContent == null)
+            {
+                throw new NullReferenceException("The owning ReqIFContent of the SpecRelationType is not set.");
+            }
+
+            var specRelations = specRelationType.ReqIFContent.SpecRelations;
+
+            var result = specRelations.Where(x => x.Type == specRelationType);
+
+            return result;
+        }
+    }
+}
