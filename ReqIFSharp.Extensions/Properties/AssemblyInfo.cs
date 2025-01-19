4a81dcdc417449455ad5575f8a69060bef2843c6
samatstarion <s.gerene@stariongroup.eu>
2025-01-19 20:25:08 +0100

    [Update] copyright statement to 2025

--- ReqIFSharp.Extensions/Properties/AssemblyInfo.cs
+++ ReqIFSharp.Extensions/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿// -------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="Starion Group S.A.">
 //
-//   Copyright 2017-2024 Starion Group S.A.
+//   Copyright 2017-2025 Starion Group S.A.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
